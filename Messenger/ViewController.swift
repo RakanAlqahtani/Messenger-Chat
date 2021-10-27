2c367d8794abae40c325b8f32400aa5f271801ff
rakan alqahtani <rakanalqahtani98@gmail.com>
2021-10-27 17:46:00 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Rakan Alqahtani  on 21/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
