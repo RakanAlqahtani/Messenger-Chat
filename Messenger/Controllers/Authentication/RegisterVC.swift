59329b4b8ae35c592f20f1cf527edaf61790c803
rakan alqahtani <rakanalqahtani98@gmail.com>
2021-10-31 15:28:34 +0300

    Section 3 - Photo Upload

--- Messenger/Controllers/Authentication/RegisterVC.swift
+++ Messenger/Controllers/Authentication/RegisterVC.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class RegisterVC: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +16,11 @@ class RegisterVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func seletcPhotoAction(_ sender: UITapGestureRecognizer) {
+        presentPhotoPicker()
+        print(")000000")
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -26,4 +31,53 @@ class RegisterVC: UIViewController {
     }
     */
 
+}
+
+
+extension RegisterVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // get results of user taking picture or selecting from camera roll
+    func presentPhotoActionSheet(){
+        let actionSheet = UIAlertController(title: "Profile Picture", message: "How would you like to select a picture?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { [weak self] _ in
+            self?.presentCamera()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { [weak self] _ in
+            self?.presentPhotoPicker()
+        }))
+        
+        present(actionSheet, animated: true)
+    }
+    func presentCamera() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    func presentPhotoPicker() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // take a photo or select a photo
+        
+        // action sheet - take photo or choose photo
+        picker.dismiss(animated: true, completion: nil)
+        print(info)
+        
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        
+        self.imageView.image = selectedImage
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
